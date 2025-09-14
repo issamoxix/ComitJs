0be851bc608e737b3312931c6f19d89cc941bcbf
Issam Haidaoui <issamhaidaoui@gmail.com>
2025-09-14 21:12:35 +0200

    refactor: implement centralized function to fetch commit messages from API

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 const { exec } = require("child_process");
 const { Select } = require("enquirer");
 
@@ -6,34 +7,48 @@ const actionUrl = "https://api.comit.dev/actions";
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-async function SelectComit(comitMessages) {
+async function getCommitMessage(stagedDiff) {
+  const payload = {
+    code: stagedDiff,
+  };
+  const response = await fetch(`${actionUrl}/commit?token=default`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: myHeaders,
+  });
+
+  const responseJson = await response.json();
+  const commitMessages = responseJson.message;
+  return commitMessages;
+}
+
+async function SelectComit(comitMessages, stdout = null) {
+  if (comitMessages[0].length == 0 && stdout != null) {
+    comitMessages = await getCommitMessage(stdout);
+    await SelectComit(comitMessages, stdout);
+    return;
+  }
   const prompt = new Select({
     name: "Comit",
     message: "Select commit message:",
     choices: comitMessages,
   });
 
-  const commit = await prompt.run();
-  exec(`git commit -m  "${commit}"`, (error, stdout, stderr) => {
-    if (stderr){
-        console.log(stderr)
-        console.log("Report this issue to https://comit.dev")   
-    }
-  });
+  try {
+    const commit = await prompt.run();
+    exec(`git commit -m  "${commit}"`, (error, stdout, stderr) => {
+      if (stderr) {
+        console.log(stderr);
+        console.log("Report this issue to https://comit.dev");
+      }
+    });
+  } catch {}
+  return;
 }
 
 exec("git --no-pager diff --staged", async (error, stdout, stderr) => {
   if (stdout.length > 5) {
-    const payload = {
-      code: stdout,
-    };
-    const response = await fetch(`${actionUrl}/commit?token=default`, {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: myHeaders,
-    });
-    const responseJson = await response.json();
-    const commitMessages = responseJson.message;
-    SelectComit(commitMessages);
+    const commitMessages = await getCommitMessage(stdout);
+    await SelectComit(commitMessages, stdout);
   }
 });
